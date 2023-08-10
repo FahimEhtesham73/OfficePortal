ca9eba3e801c490458195d53a614326259f6c05c
FahimEhtesham73 <fahim123ehtesham@gmail.com>
2023-08-10 14:53:29 +0600

    env added

---
+++ bash_command/noderun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the desired folder
+cd "/media/nslserver/hdd2/fullstack/Leave_Management_07_07_2023/server"
+
+# Run the Node.js script
+npm run cluster
---
+++ bash_command/reactrun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the desired folder
+cd "/media/nslserver/hdd2/fullstack/Leave_Management_07_07_2023/client"
+
+# Run the Node.js script
+npm start
